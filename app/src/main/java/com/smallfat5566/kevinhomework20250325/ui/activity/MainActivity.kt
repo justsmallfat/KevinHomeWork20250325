f2970ae430d0440376deb1a08ba797c97fc84219
smallfat <Justsmallfat@gmail.com>
2025-04-12 11:52:12 +0800

    畫面強化

--- app/src/main/java/com/smallfat5566/kevinhomework20250325/ui/activity/MainActivity.kt
+++ app/src/main/java/com/smallfat5566/kevinhomework20250325/ui/activity/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.smallfat5566.kevinhomework20250325.ui.activity
 
 import android.os.Bundle
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import com.smallfat5566.kevinhomework20250325.databinding.ActivityMainBinding
 
 class MainActivity : AbstractActivity() {
@@ -12,6 +16,17 @@ class MainActivity : AbstractActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    // 啟用 Edge-to-Edge
+        WindowCompat.setDecorFitsSystemWindows(window, false)
 
+        // 處理系統欄的 Insets
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { view, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.updatePadding(
+                top = systemBars.top, // 狀態列高度
+                bottom = systemBars.bottom // 導航列高度
+            )
+            insets // 返回 insets 以便其他視圖處理
+        }
     }
 }
